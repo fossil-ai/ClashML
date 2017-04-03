b2147942449e3c15b2dc966d7134f16346a9adb5
Faisal <faisalmohammad1993@gmail.com>
2017-04-03 19:31:48 -0400

    renamed key values for easier input

--- data_collection.py
+++ data_collection.py
@@ -9,18 +9,19 @@ This is the data collection module for obtaining data from Royale TV (this was d
 
 cardDict = {'mirror': 29, 'graveyard': 34, 'bombtower': 10, 'guards': 49, 'rage': 64, 'furnace': 47, 'barbarians': 40,
             'elitebarbarians': 36, 'speargoblin': 24, 'minipekka': 38, 'icegolem': 17, 'valkyrie': 61, 'threemusk': 51, 'firespirits': 46,
-            'cannon': 9, 'frieball': 58, 'clone': 8, 'poison': 2, 'rocket': 11, 'skeletons': 41, 'battleram': 37, 'musketeer': 19,
+            'cannon': 9, 'fireball': 58, 'clone': 8, 'poison': 2, 'rocket': 11, 'skeletons': 41, 'battleram': 37, 'musketeer': 19,
             'megaminion': 65, 'log': 21, 'hogrider': 27, 'collector': 52, 'heal': 66, 'knight': 73, 'bomber': 6, 'icespirit': 5,
-            'wizard': 57, 'nightprincess': 4, 'tombstone': 56, 'tornado': 22, 'executioner': 31, 'bandit': 48, 'infernotower': 67,
+            'wizard': 57, 'darkprincess': 4, 'tombstone': 56, 'tornado': 22, 'executioner': 31, 'bandit': 48, 'infernotower': 67,
             'icewizard': 45, 'archer': 54, 'balloon': 60, 'miner': 26, 'royalgiant': 3, 'golem': 71, 'darkprince': 23, 'mortar': 0,
             'arrows': 72, 'bats': 16, 'nightwitch': 70, 'electrowizard': 55, 'witch': 69, 'giantpekka': 35, 'barbarianhut': 30,
             'lightning': 42, 'zap': 32, 'minions': 28, 'minionhorde': 63, 'dartgoblin': 44, 'goblinhut': 68, 'infernodragon': 18,
             'sparky': 39, 'tesla': 59, 'prince': 14, 'lavahound': 62, 'skeletonarmy': 33, 'babydragon': 50, 'goblinbarrel': 15,
-            'giantskeleton': 25, 'freeze': 43, 'lumberjack': 13, 'xbow': 1, 'bowler': 12, 'goblingang': 20, 'princess': 7, 'goblins': 53}
+            'giantskeleton': 25, 'freeze': 43, 'lumberjack': 13, 'xbow': 1, 'bowler': 12, 'goblingang': 20, 'princess': 7, 'goblins': 53,
+            'giant':74}
 
 ####### CREATE DATA ARRAY #######
 
-data = [0] * 149
+data = [0] * 151
 
 ####### INPUT MANUAL DATA #######
 
@@ -44,7 +45,7 @@ for x in card_x:
 for y in card_y:
 	data[cardDict[y] + 74] = 1
 
-data[148] = winner
+data[150] = winner
 
 file = open("data","a")
 file.write(str(data).strip('[]') + "\n")
