334af6c615e5409b8413d1d3ca0a9d76f5d2c282
Faisal <faisalmohammad1993@gmail.com>
2017-04-08 00:07:39 -0400

    clashNN.py divides the data into a training set and test set

--- clashNN.py
+++ clashNN.py
@@ -9,14 +9,19 @@ X_samples_shape = (1,32)
 
 def load_dataset():
 	data = np.loadtxt('attr_data', delimiter=',')
-	Y_train = data[:,data.shape[1] - 1]
-	X_train = sp.delete(data, data.shape[1] - 1, axis=1)
+	half = int((data.shape[0]) / 2)
+	Y_train = data[:half,data.shape[1] - 1]
+	Y_test = data[half :, data.shape[1] - 1]
+	X_data = sp.delete(data, data.shape[1] - 1, axis=1)
+	X_train = X_data[half:]
+	X_test = X_data[:half]
 	X_train = X_train.reshape(-1,X_samples_shape[0],X_samples_shape[1])
+	X_test = X_test.reshape(-1, X_samples_shape[0], X_samples_shape[1])
 	Y_train = Y_train.astype(np.uint8)
-	print(X_train)
-	return X_train, Y_train
+	Y_test = Y_test.astype(np.uint8)
+	return X_train, Y_train, X_test, Y_test
 
-X_train, Y_train = load_dataset()
+X_train, Y_train, X_test, Y_test = load_dataset()
 
 net1 = NeuralNet(
     layers=[('input', layers.InputLayer),
@@ -28,12 +33,12 @@ net1 = NeuralNet(
     # input layer
     input_shape=(None, 1, X_samples_shape[1]),
     # dropout1
-    dropout1_p=0.5,
+    dropout1_p=0.75,
     # dense
     dense_num_units=256,
     dense_nonlinearity=lasagne.nonlinearities.rectify,
     # dropout2
-    dropout2_p=0.5,
+    dropout2_p=0.75,
     # output
     output_nonlinearity=lasagne.nonlinearities.softmax,
     output_num_units=2,
@@ -41,11 +46,13 @@ net1 = NeuralNet(
     update=nesterov_momentum,
     update_learning_rate=0.001,
     update_momentum=0.9,
-    max_epochs=500,
+    max_epochs=250,
     verbose=1,
     )
 # Train the network
 nn = net1.fit(X_train, Y_train)
+
+print("CHECK AGAINST TRAINING DATA")
 
 preds = net1.predict(X_train)
 
@@ -56,3 +63,15 @@ for x,y in zip(preds,Y_train):
 		score+=1
 
 print(score/len(Y_train))
+
+print("CHECK AGAINST TEST DATA")
+
+preds = net1.predict(X_test)
+
+score = 0
+for x,y in zip(preds,Y_test):
+	print(x,y)
+	if x == y:
+		score+=1
+
+print(score/len(Y_test))
